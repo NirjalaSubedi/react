32c3b8846053c004d5591a671e02f8f3988e519a
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-09-02 09:07:21 +0545

    comprasion operator

--- javascript/day-2.js
+++ javascript/day-2.js
@@ -2,3 +2,17 @@
 let a=6;
 let b=4;
 console.log(a%b);
+
+//comparison operator
+const data1="hello";
+const data2="hello";
+console.log(data1===data2);
+console.log("====================");
+
+const num1=8;
+const num2=9;
+console.log(num1>num2);
+console.log(num1<num2);
+console.log(num1>=num2);
+console.log(num1<=num2);
+console.log(num1!=num2);
