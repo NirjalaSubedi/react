9fddcfbbbc85c5977ada3457715ded4a103d7c70
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-09-02 09:19:18 +0545

    logical operator

--- javascript/day-2.js
+++ javascript/day-2.js
@@ -16,3 +16,16 @@ console.log(num1<num2);
 console.log(num1>=num2);
 console.log(num1<=num2);
 console.log(num1!=num2);
+
+console.log("====================");
+//logical operator
+let age=20;
+let value1=age>18 && age<60;
+console.log(`can vote:${value1}`);
+
+let value2=age<18||age>60;
+console.log(`can not vote:${value2}`);
+
+let value3=!value2;
+console.log(`can vote:${value3}`);
+console.log("====================");
